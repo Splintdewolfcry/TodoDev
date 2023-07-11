b9e5a8ae4593b27f950b34a3e06b56a9c9b77ad8
Splintdewolfcry <spymaccry@gmail.com>
2023-07-11 19:38:22 +0300

    Day 7 Complete
    Edit needs work, it's not currently working properly
    Maybe start writing unit tests?

--- match.py
+++ match.py
@@ -5,25 +5,59 @@ while True:
 
     match user_action:
         case 'add':
-            todo = input('Enter a to-do: ')
+            todo = input('Enter a to-do: ') + '\n'
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            
             todos.append(todo)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
+                item.strip('\n')
                 row = f'[{index + 1}] - {item}'
                 print(row)
         case 'edit':
+            #show the items
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             for index, item in enumerate(todos):
+                item.strip('\n')
                 row = f'[{index + 1}] - {item}'
                 print(row)
+            
+            #Ask which one you would like to edit
+            user_edit_number = int(input('Number of the todo to edit: '))
+            user_edit_number = user_edit_number - 1
+            
+            #Input the change
+            overriden_todo = input('Input new todo: ')
 
-                number = int(input('Number of the todo to edit: '))
-                number = number - 1
-                new_todo = input('Input new todo')
-                todos[number] = new_todo
+            with open('todos.txt', 'w') as file:
+                todos = file.writelines(todos[overriden_todo])
+            
+                
+            
+            
+            
+            
+            
+            # for index, item in enumerate(todos):
+            #     row = f'[{index + 1}] - {item}'
+            #     print(row)
+            #     for todo in todos:
+            #         number = int(input('Number of the todo to edit: '))
+            #         number = number - 1
+            #         new_todo = input('Input new todo')
+            #         todos[number] = new_todo
         case 'complete':
             number = int(input('Which one did you complete?  '))
             todos.pop(number)
